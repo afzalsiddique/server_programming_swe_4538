e169105498d456aa8ab062ac67e9315bb36700b3
afzal <afzal.siddique99@gmail.com>
2021-05-07 01:37:05 +0600

    load content and endpoints

--- Lab_1_180042112/http.js
+++ Lab_1_180042112/http.js
@@ -1,44 +1,48 @@
 const http = require('http')
-const data = require('./loadContent')
+const json_obj = require('./loadContent')
 
-
+const index = json_obj.data.index
+const about = json_obj.data.about
+const blog = json_obj.data.blog
+const contact = json_obj.data.contact
+const pricing = json_obj.data.pricing
+const service = json_obj.data.service
+const work = json_obj.data.work
 
 const final_server = http.createServer((request,response) => {
     
-    // response.writeHead(201,{'Content-Type': "text/plain"})
+    response.writeHead(201,{'Content-Type': "text/html"})
 
-    // response.write("<h1>Hello</h1>")
-    // response.end()
     if(request.url=="/"){
-        response.write(data.loadData.index)
+        response.write(index)
         response.end()
     }
     else if (request.url=="/about"){
-        response.write(data.loadData.about)
+        response.write(about)
         response.end()
     }
     else if (request.url=="/blog"){
-        response.write(data.loadData.blog)
+        response.write(blog)
         response.end()
     }
     else if (request.url=="/contact"){
-        response.write(data.loadData.contact)
+        response.write(contact)
         response.end()
     }
     else if (request.url=="/pricing"){
-        response.write(data.loadData.pricing)
+        response.write(pricing)
         response.end()
     }
     else if (request.url=="/services"){
-        response.write(data.loadData.service)
+        response.write(service)
         response.end()
     }
     else if (request.url=="/work"){
-        response.write(data.loadData.work)
+        response.write(work)
         response.end()
     } 
     else {
-        response.write("<h2>This page don't exist</h2> <a href = '/'>Go To base</a>")
+        response.write("<h2>This page doesn't exist</h2> <a href = '/'>Go To base</a>")
         response.end()
     }
 })
--- Lab_1_180042112/loadContent.js
+++ Lab_1_180042112/loadContent.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 
-const aboutData = fs.readFileSync('./contents/HTML/about.html',"utf-8")
-const blogData = fs.readFileSync('./contents/HTML/blog.html',"utf-8")
-const contactData = fs.readFileSync('./contents/HTML/contact.html',"utf-8")
-const indexData = fs.readFileSync('./contents/HTML/index.html',"utf-8")
-const pricingData = fs.readFileSync('./contents/HTML/pricing.html',"utf-8")
-const serviceData = fs.readFileSync('./contents/HTML/services.html',"utf-8")
+const about = fs.readFileSync('./contents/HTML/about.html',"utf-8")
+const blog = fs.readFileSync('./contents/HTML/blog.html',"utf-8")
+const contact = fs.readFileSync('./contents/HTML/contact.html',"utf-8")
+const index = fs.readFileSync('./contents/HTML/index.html',"utf-8")
+const pricing = fs.readFileSync('./contents/HTML/pricing.html',"utf-8")
+const service = fs.readFileSync('./contents/HTML/services.html',"utf-8")
 const workData = fs.readFileSync('./contents/HTML/work.html',"utf-8")
 
-const loadData = {
-    'about':aboutData,
-    'blog':blogData,
-    'contact': contactData,
-    'index': indexData,
-    'pricing': pricingData,
-    'service': serviceData,
+const data = {
+    'about':about,
+    'blog':blog,
+    'contact': contact,
+    'index': index,
+    'pricing': pricing,
+    'service': service,
     'work': workData
 }
 
-module.exports = {loadData}
+module.exports = {data}
