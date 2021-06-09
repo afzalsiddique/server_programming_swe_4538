dab08aba42671aa27ab6b90d548e84055a6900af
afzal <afzal.siddique99@gmail.com>
2021-06-09 19:17:24 +0600

    controller video->1:52:55

---
+++ lab_2_180042112/controllers/userController.controllers.js
@@ -0,0 +1,26 @@
+const getRegister = (req,res)=>{
+    res.sendFile("register.html",{root:"./views/users/"});
+    // res.send("this is register page")
+}
+
+const postRegister = (req,res)=>{
+    // const username = req.body.username
+    // const email = req.body.email
+    // res.send(`<H1>user with E-mail ${email} and Username ${username} is requesting to register</H1>`)
+    // // res.sendFile("register.html",{root:"./views/users/"});
+
+    res.redirect("/dashboard")
+}
+
+const getLogIn = (req,res)=>{
+    // const id = req.query.id
+    // const username = req.query.username
+    const {id,username}=req.query
+    res.send(`THIS IS LOGIN PAGE. user with ID ${id} and Username ${username} is requesting to login`)
+}
+
+const getDashboard = (req,res)=>{
+    res.send('User Dashboard')
+}
+
+module.exports = {getRegister,postRegister, getLogIn, getDashboard}
--- lab_2_180042112/routes/userRoutes.routes.js
+++ lab_2_180042112/routes/userRoutes.routes.js
@@ -2,40 +2,24 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const isLoggedIn = require('./../middlewares/auth.middlewares')
+const {getRegister,postRegister, getLogIn, getDashboard} = require('./../controllers/userController.controllers')
 
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
 
-router.get("/login", (req,res)=>{
-    // const id = req.query.id
-    // const username = req.query.username
-    const {id,username}=req.query
-    res.send(`THIS IS LOGIN PAGE. user with ID ${id} and Username ${username} is requesting to login`)
-})
+router.get("/login", getLogIn)
 
-router.get("/dashboard/:id/:username", (req,res)=>{
-    const id = req.params.id
-    const username = req.params.username
+// router.get("/dashboard/:id/:username", (req,res)=>{
+//     const id = req.params.id
+//     const username = req.params.username
 
-    // localhost:7777/users/dashboard/7/afzal
-    res.send( `user with ID ${id} and Username ${username} is requesting to access dashboard`) 
-})
+//     // localhost:7777/users/dashboard/7/afzal
+//     res.send( `user with ID ${id} and Username ${username} is requesting to access dashboard`) 
+// })
 
-router.get("/dashboard",(req,res)=>{
-    res.send('User Dashboard')
-})
+router.get("/dashboard", getDashboard)
 
-router.get("/register", (req,res)=>{
-    res.sendFile("register.html",{root:"./views/users/"});
-    // res.send("this is register page")
-})
-router.post("/register", isLoggedIn,(req,res)=>{
-    // const username = req.body.username
-    // const email = req.body.email
-    // res.send(`<H1>user with E-mail ${email} and Username ${username} is requesting to register</H1>`)
-    // // res.sendFile("register.html",{root:"./views/users/"});
-
-    res.redirect("/dashboard")
-})
+router.get("/register", getRegister)
+router.post("/register", postRegister)
 
 module.exports = router
