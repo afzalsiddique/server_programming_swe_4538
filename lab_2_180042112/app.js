12b28f950c9814f3e0b1751449a96a37b6076e0c
afzal <afzal.siddique99@gmail.com>
2021-06-09 15:09:27 +0600

    more pages created (video-> 23:36)

--- lab_2_180042112/app.js
+++ lab_2_180042112/app.js
@@ -4,5 +4,13 @@ const app = express()
 app.get("/", (req,res)=>{
     res.send("<H1>Homepage</H1>")
 })
-
+app.get("/about", (req,res)=>{
+    res.send("<H1>About</H1>")
+})
+app.get("/contact", (req,res)=>{
+    res.send("<H1>Contact</H1>")
+})
+app.use((req,res)=>{
+    res.send("error 404. Page does not exist")
+})
 module.exports = app;
