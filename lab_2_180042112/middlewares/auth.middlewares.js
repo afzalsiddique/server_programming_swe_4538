1080afdac8d21090389da401a0275f05ba8d73c3
afzal <afzal.siddique99@gmail.com>
2021-06-09 18:50:03 +0600

    router middleware video->1:45:50

---
+++ lab_2_180042112/middlewares/auth.middlewares.js
@@ -0,0 +1,11 @@
+const isLoggedIn = (req,res,next)=>{
+    const username = req.body.username
+    const email = req.body.email
+    if (username=="admin"){
+        next()
+    }else{
+        res.redirect("/login")
+    }
+}
+
+module.exports = isLoggedIn
--- lab_2_180042112/routes/userRoutes.routes.js
+++ lab_2_180042112/routes/userRoutes.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
+const isLoggedIn = require('./../middlewares/auth.middlewares')
 
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
@@ -9,25 +10,32 @@ router.get("/login", (req,res)=>{
     // const id = req.query.id
     // const username = req.query.username
     const {id,username}=req.query
-    res.send(`user with ID ${id} and Username ${username} is requesting to login`)
+    res.send(`THIS IS LOGIN PAGE. user with ID ${id} and Username ${username} is requesting to login`)
 })
 
 router.get("/dashboard/:id/:username", (req,res)=>{
     const id = req.params.id
     const username = req.params.username
+
     // localhost:7777/users/dashboard/7/afzal
     res.send( `user with ID ${id} and Username ${username} is requesting to access dashboard`) 
+})
+
+router.get("/dashboard",(req,res)=>{
+    res.send('User Dashboard')
 })
 
 router.get("/register", (req,res)=>{
     res.sendFile("register.html",{root:"./views/users/"});
     // res.send("this is register page")
 })
-router.post("/register", (req,res)=>{
-    const username = req.body.username
-    const email = req.body.email
-    res.send(`<H1>user with E-mail ${email} and Username ${username} is requesting to register</H1>`)
-    // res.sendFile("register.html",{root:"./views/users/"});
+router.post("/register", isLoggedIn,(req,res)=>{
+    // const username = req.body.username
+    // const email = req.body.email
+    // res.send(`<H1>user with E-mail ${email} and Username ${username} is requesting to register</H1>`)
+    // // res.sendFile("register.html",{root:"./views/users/"});
+
+    res.redirect("/dashboard")
 })
 
 module.exports = router
