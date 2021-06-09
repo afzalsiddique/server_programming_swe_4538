41ebf3004370eea770f2ce61c4f13ed7fd65c00b
afzal <afzal.siddique99@gmail.com>
2021-06-09 15:31:53 +0600

    saving cookie 32:12

--- lab_2_180042112/app.js
+++ lab_2_180042112/app.js
@@ -8,6 +8,8 @@ app.post("/", (req,res)=>{
     res.send("<H1>Homepage - post request</H1>")
 })
 app.get("/about", (req,res)=>{
+    res.cookie("dummy_cookie","afzal")
+    // res.clearCookie("dummy_cookie")
     res.send("<H1>About</H1>")
 })
 app.get("/contact", (req,res)=>{
