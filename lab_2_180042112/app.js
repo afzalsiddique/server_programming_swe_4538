dcd6212abf5eed3e9fb7624591a9aafc67984e87
afzal <afzal.siddique99@gmail.com>
2021-06-09 15:23:40 +0600

    sending post req (video -> 29:18)

--- lab_2_180042112/app.js
+++ lab_2_180042112/app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req,res)=>{
-    res.send("<H1>Homepage</H1>")
+    res.send("<H1>Homepage - get request</H1>")
+})
+app.post("/", (req,res)=>{
+    res.send("<H1>Homepage - post request</H1>")
 })
 app.get("/about", (req,res)=>{
     res.send("<H1>About</H1>")
@@ -11,6 +14,6 @@ app.get("/contact", (req,res)=>{
     res.send("<H1>Contact</H1>")
 })
 app.use((req,res)=>{
-    res.send("error 404. Page does not exist")
+    res.status(401).send("error 404. Page does not exist")
 })
 module.exports = app;
