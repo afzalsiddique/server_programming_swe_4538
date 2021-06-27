03b336c7e6957c2c01854a82737f1abea25b5833
afzal <afzal.siddique99@gmail.com>
2021-06-27 17:31:29 +0600

    retrieving password from database

--- lab_2_3_task2/middlewares/auth.middlewares.js
+++ lab_2_3_task2/middlewares/auth.middlewares.js
@@ -1,4 +1,5 @@
 const con = require('../mysql_db_connection')
+const bcrypt = require('bcrypt')
 const isLoggedIn = (req,res,next)=>{
     if (req.method=="POST"){
         const email = req.body.email
@@ -6,28 +7,24 @@ const isLoggedIn = (req,res,next)=>{
         var flag=false;
         con.query(`SELECT password FROM auth_table where email='${email}'`, function (err, result, fields) {
             if (err) throw err;
-            const retrieved_pass =JSON.parse(JSON.stringify(result))[0].password
-            if (password===retrieved_pass)
-                flag=true
-            else
-                flag=false
-            if (flag)
-                res.send(`
-                            <head>
-                                <meta charset='utf-8'>
-                                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                                <meta name='viewport' content='width=device-width, initial-scale=1'>
-                                <title>Dashboard</title>
-                            </head>
-                            <body>
-                                <H1> Dashboard  for ${email}</H1>
-                                
-                            </body>
-                        `
-                        )
-                // res.send(`Dashboard for ${email}`)
-            else
-                res.redirect('/login')
+            const hash =JSON.parse(JSON.stringify(result))[0].password
+
+            bcrypt.compare(password, hash, function(err, result) {
+                console.log(password)
+                console.log(hash)
+                if (result==true){
+                    res.send(` <head> <meta charset='utf-8'>
+                                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                                    <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                    <title>Dashboard</title> </head>
+                                <body> <H1> Dashboard  for ${email}</H1> </body> `)
+                }
+                else if(result==false){
+                    console.log('i came here')
+                    res.redirect('/login')
+                }
+            });
+
         });
     }else{
         next()
