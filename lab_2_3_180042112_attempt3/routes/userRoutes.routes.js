488fbf5a475acbdbde31882d8596501400002a51
afzal <afzal.siddique99@gmail.com>
2021-06-28 12:03:53 +0600

    login hoy na ☹

--- lab_2_3_180042112_attempt3/routes/userRoutes.routes.js
+++ lab_2_3_180042112_attempt3/routes/userRoutes.routes.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-const isLoggedIn = require('./../middlewares/auth.middlewares')
-const {getRegister,postRegister, getLogIn, getDashboard} = require('./../controllers/userController.controllers')
+const express = require('express');
+const router = express.Router();
+const bodyParser = require("body-parser");
+const {getRegister,postRegister,getLogin,getDashboard,postLogin} = require("./../controllers/userController.controllers");
 
-router.use(bodyParser.urlencoded({extended: false}))
-router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false}));
+router.use(bodyParser.json());
 
-router.get("/login", getLogIn)
+router.get("/login",getLogin);
 
-router.get("/dashboard", getDashboard)
+router.get("/register",getRegister);
 
-router.get("/register", getRegister)
-router.post("/register",  postRegister)
+router.post("/register",postRegister);
 
-module.exports = router
+router.post("/login",postLogin);
+
+router.get("/dashboard",getDashboard);
+
+module.exports = router;
