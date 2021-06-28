ce228486c5f8f8ee578f70a5de0195aa9b3089e6
afzal <afzal.siddique99@gmail.com>
2021-06-28 22:51:45 +0600

    slight change

--- lab_2_3_task/middlewares/auth.middlewares.js
+++ lab_2_3_task/middlewares/auth.middlewares.js
@@ -17,7 +17,10 @@ const isLoggedIn =(req,res,next)=>{
                 if(!err)
                 {
                     console.log('not error')
-                    res.send("<H1>asdf</H1>")
+                    res.send(` <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'> <title>Dashboard</title> </head>
+                            <body> <H1> Dashboard  for ${email}</H1> </body> `
+                    )
                 }
                 else{
                     res.send("invalid password");
