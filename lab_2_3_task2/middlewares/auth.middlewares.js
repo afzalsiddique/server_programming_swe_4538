c676eb39adc6d8eb36f56df10a9a31f826a2faee
afzal <afzal.siddique99@gmail.com>
2021-06-28 13:11:26 +0600

    working in login

--- lab_2_3_task2/middlewares/auth.middlewares.js
+++ lab_2_3_task2/middlewares/auth.middlewares.js
@@ -1,58 +1,30 @@
 const con = require('../mysql_db_connection')
-const bcrypt = require('bcrypt')
-const isLoggedIn = (req,res,next)=>{
-    if (req.method=="POST"){
-        const email = req.body.email
-        const password = req.body.password
-        var flag=false;
-        con.query(`SELECT password FROM auth_table where email='${email}'`, function (err, result, fields) {
-            if (err) throw err;
-            const hash =JSON.parse(JSON.stringify(result))[0].password
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-            bcrypt.compare(password, hash, function(err, result) {
-                console.log(password)
-                console.log(hash)
-                if (result==true){
-                    res.send(` <head> <meta charset='utf-8'>
-                                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                                    <meta name='viewport' content='width=device-width, initial-scale=1'>
-                                    <title>Dashboard</title> </head>
-                                <body> <H1> Dashboard  for ${email}</H1> </body> `)
+const isLoggedIn =(req,res,next)=>{
+    
+    const email = req.body.email;
+    const password= req.body.password;
+    con.query(`SELECT password FROM auth_table where email='${email}'`, function (err, result, fields) {
+                 if (err) throw err;
+              const retrieved_pass =JSON.parse(JSON.stringify(result))[0].password
+              console.log(password)
+              console.log(retrieved_pass)
+              bcrypt.compare(password, retrieved_pass, function(err, result) {
+                if(result == true)
+                {
+                    next();
                 }
-                else if(result==false){
-                    console.log('i came here')
-                    res.redirect('/login')
+                else{
+                    res.send("invalid password");
                 }
-            });
+               
+    
+});
 
-        });
-    }else{
-        next()
-    }
-}
+
+
+})}
+
 module.exports = isLoggedIn
-
-// const postLogin = (req,res)=>{
-//     if (login_validate(email,password)){
-//         res.redirect("/dashboard")
-//     }
-//     else{
-//         res.redirect("/login")
-//     }
-// }
-// const login_validate= (email,password)=>{
-//     var flag='initialValue';
-//     con.query(`SELECT password FROM auth_table where email='${email}'`, function (err, result, fields) {
-//         if (err) throw err;
-//         const retrieved_pass =JSON.parse(JSON.stringify(result))[0].password
-//         if (password===retrieved_pass)
-//             flag=true
-//         else
-//             flag=false
-//         if (flag){
-            
-//         }
-//         // console.log(flag) // true
-//     });
-//     // console.log(flag) // initialValue. this line executes before con.query
-// }
