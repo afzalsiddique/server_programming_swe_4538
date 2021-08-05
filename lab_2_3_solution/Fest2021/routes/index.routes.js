21a16480854ad30a348504a807bba3c997431aad
afzal <afzal.siddique99@gmail.com>
2021-08-05 11:55:46 +0600

    start lab task

--- lab_2_3_solution/Fest2021/routes/index.routes.js
+++ lab_2_3_solution/Fest2021/routes/index.routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const ensureIsAuthenticated = require("./../middlewares/auth.middleware")
+const {ensureIsAuthenticated,addUserData} = require("./../middlewares/auth.middleware")
 
 router.get("/",(req,res)=>{
-    res.send("<H1>Homepage</H1>")
+    res.render("welcome.ejs")
 })
-router.get("/dashboard",ensureIsAuthenticated,(req,res)=>{
-    res.render("dashboard.ejs")
+router.get("/dashboard",ensureIsAuthenticated,addUserData,(req,res)=>{
+    res.render("dashboard.ejs",{user:req.user})
 })
 
 module.exports=router
