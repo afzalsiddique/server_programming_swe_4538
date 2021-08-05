39063fcd18a17c6a306fced256a0583fd1141939
afzal <afzal.siddique99@gmail.com>
2021-08-05 20:02:17 +0600

    working on MathOlympiad

--- lab_2_3_solution/Fest2021/controllers/mathOlympiad.contoller.js
+++ lab_2_3_solution/Fest2021/controllers/mathOlympiad.contoller.js
@@ -3,6 +3,48 @@ const getMO = (req,res)=>{
     res.render('math-olympiad/register.ejs')
 }
 const postMO = (req,res)=>{
+    const {name,category, contact, email, institution, tshirt} = req.body
+    console.log(name)
+    console.log(category)
+    console.log(contact)
+    console.log(email)
+    console.log(institution)
+    console.log(tshirt)
+    let registrationFee=0
+    if(category==='School'){
+        registrationFee=250
+    }else if(category==='College'){
+        registrationFee=400
+    }else if(category==='University'){
+        registrationFee=500
+    }
+    const total=registrationFee
+    const paid = 0
+    const selected = false
+    let error=""
+    MathOlympiad.findOne({name:name,contact:contact}).then((participant)=>{
+        if(participant){
+            error="Participant with this name and contact number already exists"
+            console.log(error)
+            res.redirect("register")
+        }else{
+            const participant = new MathOlympiad({
+                name,category,contact,email,institution,paid,total,selected,tshirt
+            })
+            participant
+                .save()
+                .then(()=>{
+                error="Participant has been registered successfully"
+                console.log(error)
+                    res.redirect("register")
+            })
+            .catch(()=>{
+                error="An unexpected error occurred while registering participant"
+                console.log(error)
+                res.redirect("register")
+            })
+        }
+    })
     res.render('math-olympiad/register.ejs')
 }
 const getMOList = (req,res)=>{
