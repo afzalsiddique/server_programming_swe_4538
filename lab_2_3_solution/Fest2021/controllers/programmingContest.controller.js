63e468d9f0e58d90b8002366aad6c1117ac923ff
afzal <afzal.siddique99@gmail.com>
2021-09-03 00:25:52 +0600

    one email works in programmingcontest

--- lab_2_3_solution/Fest2021/controllers/programmingContest.controller.js
+++ lab_2_3_solution/Fest2021/controllers/programmingContest.controller.js
@@ -1,26 +1,18 @@
-require('dotenv').config();
-const user= process.env.UserEmail
-const pass= process.env.UserPass
-const subject = "Programming Contest registration completed successfully."
-const mailBody = "some body";
-const callbackFunc= function (err, info){
-            if (err){
-              console.log(err)
-              return
-            }
-            console.log("Sent: "+info.response)
-          }
+const ProgrammingContest = require("../models/ProgrammingContest.model");
+const nodemailer = require("nodemailer");
 
-const nodemailer = require('nodemailer')
+const senderMail = process.env.UserEmail;
+const password = process.env.UserPass;
 
 const transporter = nodemailer.createTransport({
   service: "hotmail",
   auth: {
-    user: user,
-    pass: pass
-  }
-})
-const ProgrammingContest = require("../models/ProgrammingContest.model");
+    user: senderMail,
+    pass: password,
+  },
+});
+
+const mailTemplate = (to, subject, body) => {};
 
 const getCP = (req, res) => {
   res.render("programming-contest/register.ejs", {
@@ -54,7 +46,6 @@ const postCP = (req, res) => {
   const selected = false;
   let error = "";
 
-
   ProgrammingContest.findOne({
     teamName: teamName,
     institution: institution,
@@ -66,30 +57,73 @@ const postCP = (req, res) => {
       res.redirect("/ProgrammingContest/register");
     } else {
       const team = new ProgrammingContest({
-        teamName, institution, coach, contactCoach, emailCoach, tshirtCoach, leader, contactLeader,
-        emailLeader, tshirtLeader, member1, contactMember1, emailMember1, tshirtMember1, member2,
-        contactMember2, emailMember2, tshirtMember2, total, paid, selected,
+        teamName,
+        institution,
+        coach,
+        contactCoach,
+        emailCoach,
+        tshirtCoach,
+        leader,
+        contactLeader,
+        emailLeader,
+        tshirtLeader,
+        member1,
+        contactMember1,
+        emailMember1,
+        tshirtMember1,
+        member2,
+        contactMember2,
+        emailMember2,
+        tshirtMember2,
+        total,
+        paid,
+        selected,
       });
       team
-        .save()
-        .then(() => {
-          error = "Team has been registered successfully!";
-          transporter.sendMail({ from:user, to: emailLeader, subject: subject, text: mailBody }, callbackFunc)
-          transporter.sendMail({ from:user, to: emailCoach, subject: subject, text: mailBody}, callbackFunc)
-          transporter.sendMail({ from:user, to: emailMember1, subject: subject, text: mailBody},callbackFunc)
-          transporter.sendMail({ from:user, to: emailMember2, subject: subject, text: mailBody}, callbackFunc)
+          .save()
+          .then(() => {
+            error = "Team has been registered successfully!";
+            console.log(error);
 
-          console.log(error);
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/register");
-        })
-        .catch((err) => {
-          error = "An unexpected error occured while registering team";
-          console.log(error);
-          console.log(err);
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/register");
-        });
+            const toMailList = [
+              emailCoach,
+              emailLeader,
+              emailMember1,
+              emailMember2,
+            ];
+
+            var to;
+            const subject = "Team registered successfully in Programming Contest";
+            const body = "Your team registration completed.";
+
+            for (let i = 0; i < toMailList.length; i++) {
+              to = toMailList[i];
+              const options = {
+                from: senderMail,
+                to: to,
+                subject: subject,
+                text: body,
+              };
+
+              transporter.sendMail(options, function (err, info) {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                console.log("Sent: " + info.response);
+              });
+            }
+
+            req.flash("error", error);
+            res.redirect("/ProgrammingContest/register");
+          })
+          .catch((err) => {
+            error = "An unexpected error occured while registering team";
+            console.log(error);
+            console.log(err);
+            req.flash("error", error);
+            res.redirect("/ProgrammingContest/register");
+          });
     }
   });
 };
@@ -98,85 +132,85 @@ const getCPList = (req, res) => {
   let all_team = [];
   let error = "";
   ProgrammingContest.find()
-    .then((data) => {
-      all_team = data;
-      res.render("programming-contest/list.ejs", {
-        error: req.flash("error"),
-        teams: all_team,
+      .then((data) => {
+        all_team = data;
+        res.render("programming-contest/list.ejs", {
+          error: req.flash("error"),
+          teams: all_team,
+        });
+      })
+      .catch(() => {
+        error = "Failed to retrieve data!";
+        res.render("programminh-contest/list.ejs", {
+          error: req.flash("error", error),
+          teams: all_team,
+        });
       });
-    })
-    .catch(() => {
-      error = "Failed to retrieve data!";
-      res.render("programminh-contest/list.ejs", {
-        error: req.flash("error", error),
-        teams: all_team,
-      });
-    });
 };
 const deleteCP = (req, res) => {
   let error = "";
 
   ProgrammingContest.deleteOne({ _id: req.params.id })
-    .then(() => {
-      let error = "Data has been deleted successfully!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    })
-    .catch(() => {
-      let error = "Failed to delete data";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
+      .then(() => {
+        let error = "Data has been deleted successfully!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      })
+      .catch(() => {
+        let error = "Failed to delete data";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
 };
 const paymentDoneCP = (req, res) => {
   const id = req.params.id;
 
   ProgrammingContest.findOne({ _id: id })
-    .then((team) => {
-      team.paid = team.total;
-      team
-        .save()
-        .then(() => {
-          let error = "Payment completed successfully!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        })
-        .catch(() => {
-          let error = "Data could not be updated!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        });
-    })
-    .catch(() => {
-      let error = "Data could not be updated!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
+      .then((team) => {
+        team.paid = team.total;
+        team
+            .save()
+            .then(() => {
+              let error = "Payment completed successfully!";
+              req.flash("error", error);
+              res.redirect("/ProgrammingContest/list");
+            })
+            .catch(() => {
+              let error = "Data could not be updated!";
+              req.flash("error", error);
+              res.redirect("/ProgrammingContest/list");
+            });
+      })
+      .catch(() => {
+        let error = "Data could not be updated!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
 };
 const selectCP = (req, res) => {
   const id = req.params.id;
 
   ProgrammingContest.findOne({ _id: id })
-    .then((team) => {
-      team.selected = true;
-      team
-        .save()
-        .then(() => {
-          let error = "Team has been selected successfully!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        })
-        .catch(() => {
-          let error = "Data could not be updated!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        });
-    })
-    .catch(() => {
-      let error = "Data could not be updated!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
+      .then((team) => {
+        team.selected = true;
+        team
+            .save()
+            .then(() => {
+              let error = "Team has been selected successfully!";
+              req.flash("error", error);
+              res.redirect("/ProgrammingContest/list");
+            })
+            .catch(() => {
+              let error = "Data could not be updated!";
+              req.flash("error", error);
+              res.redirect("/ProgrammingContest/list");
+            });
+      })
+      .catch(() => {
+        let error = "Data could not be updated!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
 };
 
 const editCP = (req, res) => {
@@ -184,20 +218,20 @@ const editCP = (req, res) => {
   let teamInfo;
 
   ProgrammingContest.findOne({ _id: id })
-    .then((team) => {
-      teamInfo = team;
-      // console.log(teamInfo);
-      res.render("programming-contest/editTeam.ejs", {
-        error: req.flash("error"),
-        teamInfo: team,
+      .then((team) => {
+        teamInfo = team;
+        // console.log(teamInfo);
+        res.render("programming-contest/editTeam.ejs", {
+          error: req.flash("error"),
+          teamInfo: team,
+        });
+      })
+      .catch(() => {
+        error = "Failed to update data!";
+        res.render("programming-contest/list.ejs", {
+          error: req.flash("error", error),
+        });
       });
-    })
-    .catch(() => {
-      error = "Failed to update data!";
-      res.render("programming-contest/list.ejs", {
-        error: req.flash("error", error),
-      });
-    });
 };
 
 const postEditCP = (req, res) => {
@@ -224,42 +258,42 @@ const postEditCP = (req, res) => {
   console.log("here");
 
   ProgrammingContest.updateOne(
-    {
-      _id: req.params.id,
-    },
-    {
-      $set: {
-        teamName,
-        institution,
-        coach,
-        contactCoach,
-        emailCoach,
-        tshirtCoach,
-        leader,
-        contactLeader,
-        emailLeader,
-        tshirtLeader,
-        member1,
-        contactMember1,
-        emailMember1,
-        tshirtMember1,
-        member2,
-        contactMember2,
-        emailMember2,
-        tshirtMember2,
+      {
+        _id: req.params.id,
       },
-    }
+      {
+        $set: {
+          teamName,
+          institution,
+          coach,
+          contactCoach,
+          emailCoach,
+          tshirtCoach,
+          leader,
+          contactLeader,
+          emailLeader,
+          tshirtLeader,
+          member1,
+          contactMember1,
+          emailMember1,
+          tshirtMember1,
+          member2,
+          contactMember2,
+          emailMember2,
+          tshirtMember2,
+        },
+      }
   )
-    .then(() => {
-      let error = "Data has been updated!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    })
-    .catch((err) => {
-      let error = "Data could not be updated!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
+      .then(() => {
+        let error = "Data has been updated!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      })
+      .catch((err) => {
+        let error = "Data could not be updated!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
 };
 
 module.exports = {
